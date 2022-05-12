817872d30f2a7764340f5f5daf8fc911935bf911
Truong Vu <truongvu0911nd@gmail.com>
2022-05-12 11:39:52 +0700

    fix init

--- models/encoders/__init__.py
+++ models/encoders/__init__.py
@@ -1,5 +1,4 @@
 from .model_irse import IR_50, IR_SE_50, Backbone, IR_101
 from .iresnet import iresnet100, iresnet50
-from .hrnet import HighResolutionNet
-from .pfld import AuxiliaryNet, PFLDInference
+from .hrnet import HighResolutionNet, get_hr_net
 from .hopenet import Hopenet
