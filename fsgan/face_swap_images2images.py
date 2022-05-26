46fdfc5964abf4b5cae9886b5c7f1d764ea34076
Truong Vu <truongvu0911nd@gmail.com>
2022-05-26 21:13:30 +0700

    update fsgan

--- fsgan/face_swap_images2images.py
+++ fsgan/face_swap_images2images.py
@@ -8,19 +8,22 @@ from tqdm import tqdm
 from glob import glob
 from PIL import Image
 import torch
-from . import landmark_transforms
-from .utils import utils as utils
-from .utils.bbox_utils import scale_bbox
-from .utils.seg_utils import blend_seg_pred
-from .utils.obj_factory import obj_factory
-from .utils.video_utils import extract_landmarks_bboxes_euler_from_images
-from .utils.img_utils import rgb2tensor, tensor2rgb
-from .utils.bbox_utils import scale_bbox, crop_img, get_main_bbox
+# import sys
+# sys.path.append("..")
+from fsgan import landmark_transforms
+from fsgan.utils import utils as utils
+from fsgan.utils.bbox_utils import scale_bbox
+from fsgan.utils.seg_utils import blend_seg_pred
+from fsgan.utils.obj_factory import obj_factory
+from fsgan.utils.video_utils import extract_landmarks_bboxes_euler_from_images
+from fsgan.utils.img_utils import rgb2tensor, tensor2rgb
+from fsgan.utils.bbox_utils import scale_bbox, crop_img, get_main_bbox
 
 import torch 
 import torch.nn as nn 
 import torch.nn.functional as F
 import torchvision.transforms.functional as TF
+import torchvision.transforms as transforms
 
 
 def unnormalize(tensor, mean, std):
@@ -93,16 +96,6 @@ class FSGAN(nn.Module):
         inpainting_model_path='fsgan/weights/ijbc_msrunet_256_2_0_inpainting_v1.pth',
         blend_model_path='fsgan/weights/ijbc_msrunet_256_2_0_blending_v1.pth',
         pose_model_path='fsgan/weights/hopenet_robust_alpha1.pth',
-        pil_transforms1=('landmark_transforms.FaceAlignCrop', 'landmark_transforms.Resize(256)',
-                        'landmark_transforms.Pyramids(2)'),
-        pil_transforms2=('landmark_transforms.FaceAlignCrop', 'landmark_transforms.Resize(256)',
-                        'landmark_transforms.Pyramids(2)', 'landmark_transforms.LandmarksToHeatmaps'),
-        tensor_transforms1=('landmark_transforms.ToTensor()',
-                            'transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5])'),
-        tensor_transforms2=('landmark_transforms.ToTensor()',
-                            'transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5])'),
-        output_path=None, min_radius=2.0, crop_size=256, reverse_output=False, verbose=0, output_crop=False,
-        display=False
     ):
         super().__init__()
         # Initialize models
@@ -111,117 +104,78 @@ class FSGAN(nn.Module):
         self.device = device
 
         # Load face reenactment model
+        print('Loading face reenactment model: "' + os.path.basename(reenactment_model_path) + '"...')
         self.Gr = obj_factory(arch).to(device)
         checkpoint = torch.load(reenactment_model_path)
         self.Gr.load_state_dict(checkpoint['state_dict'])
         self.Gr.eval()
 
         # Load face segmentation model
-        if seg_model_path is not None:
-            print('Loading face segmentation model: "' + os.path.basename(seg_model_path) + '"...')
-            if seg_model_path.endswith('.pth'):
-                checkpoint = torch.load(seg_model_path)
-                self.Gs = obj_factory(checkpoint['arch']).to(device)
-                self.Gs.load_state_dict(checkpoint['state_dict'])
-            else:
-                self.Gs = torch.jit.load(seg_model_path, map_location=device)
-            if self.Gs is None:
-                raise RuntimeError('Failed to load face segmentation model!')
-            self.Gs.eval()
-        else:
-            self.Gs = None
+        print('Loading face segmentation model: "' + os.path.basename(seg_model_path) + '"...')
+        checkpoint = torch.load(seg_model_path)
+        self.Gs = obj_factory(checkpoint['arch']).to(device)
+        self.Gs.load_state_dict(checkpoint['state_dict'])
 
         # Load face inpainting model
-        if inpainting_model_path is not None:
-            print('Loading face inpainting model: "' + os.path.basename(inpainting_model_path) + '"...')
-            if inpainting_model_path.endswith('.pth'):
-                checkpoint = torch.load(inpainting_model_path)
-                self.Gi = obj_factory(checkpoint['arch']).to(device)
-                self.Gi.load_state_dict(checkpoint['state_dict'])
-            else:
-                self.Gi = torch.jit.load(inpainting_model_path, map_location=device)
-            if self.Gi is None:
-                raise RuntimeError('Failed to load face segmentation model!')
-            self.Gi.eval()
-        else:
-            self.Gi = None
+        print('Loading face inpainting model: "' + os.path.basename(inpainting_model_path) + '"...')
+        checkpoint = torch.load(inpainting_model_path)
+        self.Gi = obj_factory(checkpoint['arch']).to(device)
+        self.Gi.load_state_dict(checkpoint['state_dict'])
 
         # Load face blending model
+        print('Loading face blending model: "' + os.path.basename(blend_model_path) + '"...')
         checkpoint = torch.load(blend_model_path)
         self.Gb = obj_factory(checkpoint['arch']).to(device)
         self.Gb.load_state_dict(checkpoint['state_dict'])
         self.Gb.eval()
 
         # Initialize transformations
-        pil_transforms1 = obj_factory(pil_transforms1) if pil_transforms1 is not None else []
-        pil_transforms2 = obj_factory(pil_transforms2) if pil_transforms2 is not None else []
-        tensor_transforms1 = obj_factory(tensor_transforms1) if tensor_transforms1 is not None else []
-        tensor_transforms2 = obj_factory(tensor_transforms2) if tensor_transforms2 is not None else []
-        self.img_transforms1 = landmark_transforms.ComposePyramids(pil_transforms1 + tensor_transforms1)
-        self.img_transforms2 = landmark_transforms.ComposePyramids(pil_transforms2 + tensor_transforms2)
+        self.img_transforms1 = landmark_transforms.ComposePyramids([
+            landmark_transforms.FaceAlignCrop(),
+            landmark_transforms.Resize(256),
+            landmark_transforms.Pyramids(2),
+            landmark_transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5])
+        ])
+        
+        self.img_transforms2 = landmark_transforms.ComposePyramids([
+            landmark_transforms.FaceAlignCrop(),
+            landmark_transforms.Resize(256),
+            landmark_transforms.Pyramids(2),
+            landmark_transforms.LandmarksToHeatmaps(),
+            landmark_transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5])
+        ])
 
     def _extract_landmarks_bboxes_euler_from_images(self, imgs):
-        # frame_indices = []
-        # landmarks = []
-        # bboxes = []
-
-        # # For each image in the directory
-        # for i, img in imgs:
-        #     img_rgb = img.permute(2, 0, 1).cpu().numpy()
-        #     detected_faces = self.fa.face_detector.detect_from_image(img_rgb.copy())
-
-        #     # Skip current frame there if no faces were detected
-        #     if len(detected_faces) == 0:
-        #         continue
-        #     curr_bbox = get_main_bbox(np.array(detected_faces)[:, :4], img_rgb.shape[:2])
-        #     detected_faces = [curr_bbox]
-
-        #     preds = self.fa.get_landmarks(img_rgb, detected_faces)
-        #     curr_landmarks = preds[0]
-        #     # curr_bbox = detected_faces[0][:4]
-
-        #     # Convert bounding boxes format from [min, max] to [min, size]
-        #     curr_bbox[2:] = curr_bbox[2:] - curr_bbox[:2] + 1
-
-        #     # Append to list
-        #     frame_indices.append(i)
-        #     landmarks.append(curr_landmarks)
-        #     bboxes.append(curr_bbox)
-
-        # detected_faces = self.fa.face_detector.de
-
-        # # Convert to numpy array format
-        # frame_indices = np.array(frame_indices)
-        # landmarks = np.array(landmarks)
-        # bboxes = np.array(bboxes)
-
-        # return landmarks, bboxes
         landmarks, _, bboxes = self.fa.get_landmarks_from_batch(imgs, return_bboxes=True)
-        landmarks, bboxes = np.array(landmarks), np.array(bboxes)
+        landmarks, bboxes = np.array(landmarks).squeeze(), np.array(bboxes).squeeze()[:, :4]
         return landmarks, bboxes
 
     def forward(self, img1, img2):
         device = self.device
         source_landmarks, source_bboxes = \
-            self._extract_landmarks_bboxes_euler_from_images(img1)
+            self._extract_landmarks_bboxes_euler_from_images(img1*255.)
 
         target_landmarks, target_bboxes = \
-            self._extract_landmarks_bboxes_euler_from_images(img2)
+            self._extract_landmarks_bboxes_euler_from_images(img2*255.)
         outputs = []
-        for k, img1_tensor, img2_tensor in enumerate(zip(img1, img2)):
+        for k, (img1_tensor, img2_tensor) in enumerate(zip(img1, img2)):
             # source_img_rgb = img1_tensor.permute(2, 0, 1).cpu().numpy()
-            source_img_rgb = TF.to_pil_image(img1_tensor)
+            source_img_rgb = np.array(TF.to_pil_image(img1_tensor))
+            # print(source_landmarks[k], source_bboxes[k])
             curr_source_tensor, curr_source_landmarks, curr_source_bbox = self.img_transforms1(
                 source_img_rgb, source_landmarks[k], source_bboxes[k])
 
             # target_img_rgb = img2_tensor.permute(2, 0, 1).cpu().numpy()
-            target_img_rgb = TF.to_pil_image(img2_tensor)
+            target_img_rgb = np.array(TF.to_pil_image(img2_tensor))
             curr_target_tensor, curr_target_landmarks, curr_target_bbox = self.img_transforms2(
                 target_img_rgb, target_landmarks[k], target_bboxes[k])
 
             # Face reenactment
             reenactment_input_tensor = []
             for j in range(len(curr_source_tensor)):
+                curr_source_tensor[j] = curr_source_tensor[j].to(device)
                 curr_target_landmarks[j] = curr_target_landmarks[j].to(device)
                 reenactment_input_tensor.append(torch.cat((curr_source_tensor[j], curr_target_landmarks[j]), dim=0).unsqueeze(0))
             reenactment_img_tensor, reenactment_seg_tensor = self.Gr(reenactment_input_tensor)
@@ -255,7 +209,3 @@ class FSGAN(nn.Module):
             outputs.append(render_img)
 
         return np.array(outputs)
-
-
-if __name__ == "__main__":
-    fsgan = FSGAN()
