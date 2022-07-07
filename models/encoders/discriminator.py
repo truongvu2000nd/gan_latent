90d99c882c0f661c2f0516117a518a5fb6adf31f
Truong Vu <truongvu0911nd@gmail.com>
2022-07-07 22:00:10 +0700

    discriminate only 7 first codes

--- models/encoders/discriminator.py
+++ models/encoders/discriminator.py
@@ -2,10 +2,11 @@ from torch import nn
 
 
 class LatentCodesDiscriminator(nn.Module):
-    def __init__(self, style_dim=512, n_mlp=4):
+    def __init__(self, style_dim=512, n_mlp=4, get_first_codes=7):
         super().__init__()
 
         self.style_dim = style_dim
+        self.get_first_codes = get_first_codes
 
         layers = []
         for i in range(n_mlp-1):
@@ -16,9 +17,9 @@ class LatentCodesDiscriminator(nn.Module):
         layers.append(nn.Linear(512, 1))
         self.mlp = nn.Sequential(*layers)
 
-    def forward(self, w, get_first_codes=None):
-        if w.ndim == 3 and get_first_codes:
-            w = w[:, :get_first_codes, :].view(-1, 512)
+    def forward(self, w, ):
+        if w.ndim == 3 and self.get_first_codes:
+            w = w[:, :self.get_first_codes, :].view(-1, 512)
         elif w.ndim == 3:
             w = w.view(-1, 512)
         return self.mlp(w)
